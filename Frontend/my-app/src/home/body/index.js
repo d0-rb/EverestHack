11a8b6fae5fba8560ac1053f612b90af3a658cb8
Charan <phazia.co@gmail.com>
2020-06-20 02:48:42 -0500

    added navbar and body

---
+++ Frontend/my-app/src/home/body/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './index.css'
+
+class BodyMain extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+    
+    render() {
+        return (
+            <div className= "main-body-div">
+                
+            </div>
+        )
+    }
+}
+
+export default BodyMain;
---
+++ Frontend/my-app/src/home/navbar/navbar.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './navbar.css'
+
+class NavBar extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+    
+    render() {
+        return (
+            <div className= "navbar-base-div">
+                <div className= "home-icon-div">
+
+                </div>
+            </div>
+        )
+    }
+}
+
+export default NavBar;
