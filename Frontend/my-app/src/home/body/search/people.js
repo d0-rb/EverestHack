f0d2050ac825a40d2d925f6f63020b9a638a81a2
Charan <phazia.co@gmail.com>
2020-06-21 01:21:37 -0500

    finished most of the interface
    Biggest addition was Redux and some UI libraries

--- Frontend/my-app/src/home/body/search/people.js
+++ Frontend/my-app/src/home/body/search/people.js
@@ -1,12 +1,62 @@
 import React from 'react'
+import Dialog from '@material-ui/core/Dialog';
+import { Scrollbars } from 'react-custom-scrollbars'
+import RecipeItem from './recipeitem'
 import './search.css'
 
-function PeopleItem(props) {
-    return (
-        <div className= "people-item">
-            <p className= "people-item-name">{props.name}</p>
-        </div>
-    )
+class PeopleItem extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            setOpen: false
+        }
+    }
+
+    handleClick = () => {
+        console.log("clicked")
+        this.setState({
+            setOpen: true
+        })
+    }
+
+    handleClose = () => {
+        this.setState({
+            setOpen: false
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <div onClick= {this.handleClick} className= "people-item">
+                <p className= "people-item-name">{this.props.name}</p>
+            </div>
+                <Dialog open={this.state.setOpen} onClose={this.handleClose}>
+                    <div className= "dialog-body">
+                        <div className= "dialog-header">
+                            <h1 className= "dialog-header-name">{this.props.name}</h1>
+                        </div>
+                        <div style={{overflow: "hidden", height: "100%", padding: "10px"}}>
+                            <h1 style={{margin: "0"}}>Recipes</h1>
+                            <Scrollbars style={{height: "95%"}}>
+                                <div style={{postion: "relative", left: "0px"}} className= "grid-container">
+                                    <div className= "item-grid">
+                                        <RecipeItem recipeName= "Casserole"/>
+                                        <RecipeItem recipeName= "Casserole"/>
+                                        <RecipeItem recipeName= "Casserole"/>
+                                        <RecipeItem recipeName= "Casserole"/>
+                                        <RecipeItem recipeName= "Casserole"/>
+                                        <RecipeItem recipeName= "Casserole"/>
+                                        <RecipeItem recipeName= "Casserole"/>
+                                    </div>
+                                </div>
+                            </Scrollbars>
+                        </div>
+                    </div>
+                </Dialog>
+            </div>
+        )
+    }
 }
 
 export default PeopleItem;
--- Frontend/my-app/src/home/body/search/results.js
+++ Frontend/my-app/src/home/body/search/results.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import RecipeItem from './recipeitem'
 import PeopleItem from './people'
+import { Scrollbars } from 'react-custom-scrollbars'
 import './search.css'
 
 class Results extends React.Component {
     constructor(props) {
         super(props)
+
     }  
+
 
     render() {
         let displayPeople = this.props.displayPeople
@@ -15,16 +18,17 @@ class Results extends React.Component {
                 <div className= "results-base">
                     <h1 className= "main-results-title">People</h1>
                     <div className= "separator-line"></div>
-                    <div className= "grid-container">
-                        <div className= "item-grid">
-                            <PeopleItem name= "John Smith"/>
-                            <PeopleItem name= "John Smith"/>
-                            <PeopleItem name= "John Smith"/>
-                            <PeopleItem name= "John Smith"/>
-
+                    <Scrollbars style={{height: "95%", position: "relative", left: "15px"}}>
+                        <div className= "grid-container">
+                            <div className= "people-grid">
+                                <PeopleItem name= "Test Smith"/>
+                                <PeopleItem name= "John Smith"/>
+                                <PeopleItem name= "John Smith"/>
+                                <PeopleItem name= "John Smith"/>
+                            </div>
                         </div>
-                    </div>
-
+                    </Scrollbars>
+                    
                 </div>
             )
         }
@@ -32,18 +36,19 @@ class Results extends React.Component {
             <div className= "results-base">
                 <h1 className= "main-results-title">Recipes</h1>
                 <div className= "separator-line"></div>
+                <Scrollbars style={{height: "95%", position: "relative", left: "15px"}}>
                 <div className= "grid-container">
                     <div className= "item-grid">
                         <RecipeItem recipeName= "Casserole"/>
+                        <RecipeItem recipeName= "Spagheti"/>
                         <RecipeItem recipeName= "Casserole"/>
                         <RecipeItem recipeName= "Casserole"/>
                         <RecipeItem recipeName= "Casserole"/>
                         <RecipeItem recipeName= "Casserole"/>
                         <RecipeItem recipeName= "Casserole"/>
-                        <RecipeItem recipeName= "Casserole"/>
-  
                     </div>
                 </div>
+                </Scrollbars>
             </div>
         )
         
